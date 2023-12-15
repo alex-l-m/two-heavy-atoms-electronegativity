2fdc2e6f35f9a973e5aaf077725cd08e43291bef
Alexander Davis <alexlovesmolecules@gmail.com>
2023-12-15 07:52:19 -0500

    Scripts to generate GAMESS input and run GAMESS

---
+++ make_gamess_input.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from ase import Atoms
+import ase.io
+from create_gamess_inp import parse_inp, dict2text, atoms2data
+
+field_numbers = list(range(21))
+field_values = [i / 100 - 0.1 for i in field_numbers]
+
+charges = [-1, 0, 1]
+multiplicities = [2, 1, 2]
+
+# Coordinates of each molecule
+# Column names: unique_id, formula, atomnumber, symbol, atomic_number, x, y, z
+coordinates = pd.read_csv('coordinates.csv.gz')
+
+outrows = []
+
+for formula, table in coordinates.groupby('formula'):
+    this_coordinates = []
+    atomic_numbers = []
+    symbols = []
+    for row in table.itertuples():
+        this_coordinates.append([row.x, row.y, row.z])
+        atomic_numbers.append(row.atomic_number)
+        symbols.append(row.symbol)
+    ase_atoms = Atoms(symbols, this_coordinates)
+    ase.io.write(f'xyz/{formula}.xyz', ase_atoms)
+    this_data = atoms2data(formula, this_coordinates, symbols, atomic_numbers)
+    for field_number, field_value in zip(field_numbers, field_values):
+        for charge, multiplicity in zip(charges, multiplicities):
+            combination_id = f'{formula}_{field_number:02}_{charge}'
+            outpath = f'gamess_input/{combination_id}.inp'
+            # Template for GAMESS input
+            template = parse_inp('template.inp')
+            if abs(field_value) > 0.0001:
+                template['EFIELD'] = {'EVEC(3)': str(field_value)}
+            template['CONTRL']['ICHARG'] = charge
+            template['CONTRL']['MULT'] = multiplicity
+            template_text = dict2text(template)
+            outtext = template_text + '\n' + this_data
+            with open(outpath, 'w') as outfile:
+                outfile.write(outtext)
+            outrows.append({
+                'combination_id': combination_id,
+                'formula': formula,
+                'field_number': field_number,
+                'charge': charge,
+                'gamess_input_file': outpath
+            })
+
+pd.DataFrame(outrows).to_csv('simulations.csv.gz', index=False)
--- origin.sh
+++ origin.sh
@@ -1,3 +1,23 @@
 Rscript element_properties.R
 python cccbdb_coords_to_csv.py
 Rscript mol_coords.R
+mkdir xyz
+mkdir gamess_input
+mkdir gamess_logs
+mkdir gamess_output
+python make_gamess_input.py
+# Run GAMESS on all input files and save logs
+> gamess_jobs.sh
+> copy_jobs.sh
+for INPATH in gamess_input/*.inp
+do
+    BASENAME=$(basename $INPATH)
+    COMBINATION_ID=${BASENAME%.inp}
+    OUTPATH=~/gamess/restart/$COMBINATION_ID.dat
+    OUTPUT_DIR=gamess_output
+    LOGPATH=gamess_logs/$COMBINATION_ID.log
+    echo "rungms $INPATH > $LOGPATH" >> gamess_jobs.sh
+    echo "cp $OUTPATH $OUTPUT_DIR" >> copy_jobs.sh
+done
+parallel --jobs $(nproc) < gamess_jobs.sh
+sh copy_jobs.sh
