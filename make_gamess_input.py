168d393aa3374c4e6ae7440bc74c183af610d2bf
Alexander Davis <alexlovesmolecules@gmail.com>
2023-12-18 22:58:02 -0500

    When making GAMESS input, save field value and include ions for nofield only

--- make_gamess_input.py
+++ make_gamess_input.py
@@ -6,9 +6,6 @@ from create_gamess_inp import parse_inp, dict2text, atoms2data
 field_numbers = list(range(21))
 field_values = [i / 100 - 0.1 for i in field_numbers]
 
-charges = [-1, 0, 1]
-multiplicities = [2, 1, 2]
-
 # Coordinates of each molecule
 # Column names: unique_id, formula, atomnumber, symbol, atomic_number, x, y, z
 coordinates = pd.read_csv('coordinates.csv.gz')
@@ -27,6 +24,13 @@ for formula, table in coordinates.groupby('formula'):
     ase.io.write(f'xyz/{formula}.xyz', ase_atoms)
     this_data = atoms2data(formula, this_coordinates, symbols, atomic_numbers)
     for field_number, field_value in zip(field_numbers, field_values):
+        # Simulate alternate charges for neutral molecules only
+        if abs(field_value) > 1e-6:
+            charges = [0]
+            multiplicities = [1]
+        else:
+            charges = [-1, 0, 1]
+            multiplicities = [2, 1, 2]
         for charge, multiplicity in zip(charges, multiplicities):
             combination_id = f'{formula}_{field_number:02}_{charge}'
             outpath = f'gamess_input/{combination_id}.inp'
@@ -44,6 +48,7 @@ for formula, table in coordinates.groupby('formula'):
                 'combination_id': combination_id,
                 'formula': formula,
                 'field_number': field_number,
+                'field_value': field_value,
                 'charge': charge,
                 'gamess_input_file': outpath
             })
