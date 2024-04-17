47b0cd59d10e71470c5ed49c3b4f71f0f3cc8b63
Alexander Davis <alexlovesmolecules@gmail.com>
2024-04-16 22:51:59 -0400

    Shorter names for element properties from WolframAlpha

--- element_properties.R
+++ element_properties.R
@@ -2,11 +2,11 @@ library(readr)
 library(dplyr)
 
 # Atomic number of each element
-number <- read_csv('WolframAlpha--atomic_number_of_hydrogen__carbon__nitrogen__oxygen__fluorine__silicon__phosphorus__sulfur__chlorine______2023_12_14_22_59.csv', col_names = c('1', '2', 'element', '3', 'atomic_number', '4'), col_types = cols(element = col_character(), atomic_number = col_integer(), .default = col_character())) |>
+number <- read_csv('wolframalpha_number.csv', col_names = c('1', '2', 'element', '3', 'atomic_number', '4'), col_types = cols(element = col_character(), atomic_number = col_integer(), .default = col_character())) |>
     select(element, atomic_number)
 
 # One or two letter symbol of the element, corresponding to the full element string
-symbol <- read_csv('WolframAlpha--atomic_symbol_of_hydrogen__carbon__nitrogen__oxygen__fluorine__silicon__phosphorus__sulfur__chlorine______2023_12_14_23_00.csv', col_names = c('element', '1', 'symbol', '2')) |>
+symbol <- read_csv('wolframalpha_symbol.csv', col_names = c('element', '1', 'symbol', '2')) |>
     select(symbol, element)
 
 # https://www.wolframalpha.com/input?i=coulomb%27s+constant+in+eV+*+angstrom+%2F+%28electron+charge%29%5E2
@@ -17,13 +17,13 @@ kJ2eV <- 0.01036
 
 # Table of electron affinity of each element
 # https://www.wolframalpha.com/input?i=electron+affinity+of+%7Bhydrogen%2C+carbon%2C+nitrogen%2C+oxygen%2C+fluorine%2C+silicon%2C+phosphorus%2C+sulfur%2C+chlorine%7D
-wa_ea <- read_csv('WolframAlpha--electron_affinity_of_hydrogen__carbon__nitrogen__oxygen__fluorine__silicon__phosphorus__sulfur__chlorine______2023_12_14_23_00.csv', col_names = c('rownum', '?', 'element', '?', 'electron_affinity', 'unit')) |>
+wa_ea <- read_csv('wolframalpha_ea.csv', col_names = c('rownum', '?', 'element', '?', 'electron_affinity', 'unit')) |>
     select(element, electron_affinity) |>
     mutate(electron_affinity = kJ2eV * electron_affinity)
 
 # Table of ionization potential of each element
 # https://www.wolframalpha.com/input?i=first+ionization+potential+of+%7Bhydrogen%2C+carbon%2C+nitrogen%2C+oxygen%2C+fluorine%2C+silicon%2C+phosphorus%2C+sulfur%2C+chlorine%7D
-wa_ip <- read_csv('WolframAlpha--first_ionization_potential_of_hydrogen__carbon__nitrogen__oxygen__fluorine__silicon__phosphorus__sulfur__chlorine______2023_12_14_23_01.csv', col_names = c('rownum', '?', 'element', '?', 'ionization_potential', 'unit')) |>
+wa_ip <- read_csv('wolframalpha_ip.csv', col_names = c('rownum', '?', 'element', '?', 'ionization_potential', 'unit')) |>
     select(element, ionization_potential) |>
     mutate(ionization_potential = kJ2eV * ionization_potential)
 
