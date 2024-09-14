f517d6b39282f9331d5ac50bce66762be9a7b27a
Alexander Davis <alexlovesmolecules@gmail.com>
2024-09-13 21:41:54 -0400

    Removed hardcoded path to structures, read from environment variable

--- make_structure_file_table.R
+++ make_structure_file_table.R
@@ -1,6 +1,12 @@
 # Make table of files containing optimized structures of binary materials
 
 library(tidyverse)
+library(glue)
+
+# Read the environment variable containing the path to the database of
+# structures
+environment_variable_name <- 'ELECTRONEGATIVITYSTRUCTUREPATH'
+structure_path <- Sys.getenv(environment_variable_name)
 
 structure_glob <- read_csv('structure_glob.csv', col_types = cols(
     crystal_structure = col_character(),
@@ -13,8 +19,8 @@ structure_glob <- read_csv('structure_glob.csv', col_types = cols(
 structure_files <- structure_glob |>
     group_by(crystal_structure, infile_glob, infile_regex) |>
     reframe({
-        glob_results <- Sys.glob(infile_glob)
-        match_results <- str_match(glob_results, infile_regex)
+        glob_results <- Sys.glob(glue(infile_glob))
+        match_results <- str_match(glob_results, glue(infile_regex))
         colnames(match_results) <- c('structure_file_path',
                                      'symbol_cation', 'symbol_anion')
         tbl <- as_tibble(match_results)
