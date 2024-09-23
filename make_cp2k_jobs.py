36a81cb75a6b30434f77cc4186d50da1c92c1840
Alexander Davis <alexlovesmolecules@gmail.com>
2024-09-22 22:46:07 -0400

    Set initial field increment and charge increment, instead of using default

--- make_cp2k_jobs.py
+++ make_cp2k_jobs.py
@@ -52,5 +52,5 @@ with open('cp2k_jobs.sh', 'w') as f:
         structure_path = row.structure_file_path
         assert exists(structure_path)
         structure_id = row.structure_id
-        job_command = f'python apply_potential.py {structure_id} {structure_path} {cation} {anion} --kpoints 7\n'
+        job_command = f'python apply_potential.py {structure_id} {structure_path} {cation} {anion} --kpoints 7 --initfield 0.001 --dcharge 0.01\n'
         f.write(job_command)
