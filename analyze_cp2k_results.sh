b84cb191769c6433552d00dcd9f0321266e4b560
Alexander Davis <alexlovesmolecules@gmail.com>
2024-10-16 21:22:44 -0400

    Uncommented lines accidentally commented out in analysis shell script

--- analyze_cp2k_results.sh
+++ analyze_cp2k_results.sh
@@ -1,7 +1,7 @@
 # Extraction and analysis of CP2K results
-# python compute_bader.py
-# python parse_bader.py
-# Rscript charge_energy_cp2k.R
+python compute_bader.py
+python parse_bader.py
+Rscript charge_energy_cp2k.R
 Rscript compare_charge.R
 Rscript smooth_energy.R
 Rscript smoothed_energy_plots.R
