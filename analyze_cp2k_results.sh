9c2023f43dbe1de7a200971b57069a9813633a46
Alexander Davis <alexlovesmolecules@gmail.com>
2024-11-07 21:09:53 -0500

    Comments marking sections in analysis shell script

--- analyze_cp2k_results.sh
+++ analyze_cp2k_results.sh
@@ -1,14 +1,22 @@
 # Extraction and analysis of CP2K results
+
+# Derivative cube files
 python cube_diff.py density_derivatives cube_file_path
 python cube_diff.py vdiff pot_file_path
 python cube_diff.py dhartree hartree_pot_path
 python gradient_field.py
+
+# Partial charges
 python parse_cp2k_hirshfeld.py
 python compute_bader.py
 python parse_bader.py
+
+# Analysis of charges
 Rscript charge_energy_cp2k.R
 Rscript compare_charge.R
 Rscript smooth_energy.R
 Rscript smoothed_energy_plots.R
+
+# Model fitting and analysis of parameters
 Rscript electronegativity_regression.R
 Rscript compare_structure_params.R
