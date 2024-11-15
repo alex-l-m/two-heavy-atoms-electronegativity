6d334072aa62b1220ad43f43efd5101142c0382b
Alexander Davis <alexlovesmolecules@gmail.com>
2024-11-15 18:57:41 -0500

    Include Born charges

--- compare_charge.R
+++ compare_charge.R
@@ -30,6 +30,19 @@ charge_energy <- read_csv('charge_energy.csv.gz', col_types = cols(
     electronegativity_field_discrete = col_double()
 ))
 
+# Born charges
+born_charges <- read_csv('yu_cardona_table_6.7_born_charges.csv', col_types = cols(
+    cation = col_character(),
+    anion = col_character(),
+    `e*` = col_double()
+)) |>
+    mutate(formula = glue('{cation}{anion}')) |>
+    pivot_longer(c(cation, anion), names_to = 'ion_type', values_to = 'symbol') |>
+    mutate(donor_or_acceptor = ifelse(ion_type == 'cation', 'donor', ifelse(ion_type == 'anion', 'acceptor', NA))) |>
+    mutate(born_charge = ifelse(donor_or_acceptor == 'donor', `e*`, ifelse(donor_or_acceptor == 'acceptor', -`e*`, NA))) |>
+    select(formula, symbol, donor_or_acceptor, born_charge) |>
+    mutate(crystal_structure = 'zincblende', field_value = 0)
+
 # Atomic numbers, for ordering
 atomic_numbers <- read_csv('atomic_numbers.csv', col_types = cols(
     symbol = col_character(),
@@ -48,17 +61,23 @@ formula_order <- charge_energy |>
 # Compare charges with my own charge (the "charge" column)
 comparison_tbl <- charge_energy |>
     filter(category == '3-5') |>
+    # Join Born charges
+    left_join(born_charges, by = c('formula', 'donor_or_acceptor', 'symbol', 'crystal_structure', 'field_value')) |>
     mutate(formula = factor(formula, levels = formula_order)) |>
     filter(donor_or_acceptor == 'acceptor') |>
-    # Turn the Bader and Hirshfeld charges into a single column
-    pivot_longer(cols = c(bader_charge, cp2k_hirshfeld_charge),
+    pivot_longer(cols = c(bader_charge, cp2k_hirshfeld_charge, born_charge),
                  names_to = 'charge_type', values_to = 'other_charge') |>
     # Remove the suffix to make the names of the charge types shorter
-    mutate(charge_type = str_remove(charge_type, '_charge'))
+    mutate(charge_type = str_remove(charge_type, '_charge')) |>
+    mutate(charge_type = factor(charge_type, levels = c('born', 'bader', 'cp2k_hirshfeld')))
 comparison_plot <- comparison_tbl |>
     ggplot(aes(x = charge, y = other_charge, color = crystal_structure)) +
     facet_grid(charge_type ~ formula) +
     geom_abline(intercept = 0, slope = 1, linetype = 'dashed') +
+    # x and y axes
+    geom_hline(yintercept = 0, color = 'white', linetype = 'dashed') +
+    geom_vline(xintercept = 0,  color = 'white', linetype = 'dashed') +
+
     geom_line() +
     # Points for field value 0
     geom_point(data = filter(comparison_tbl, field_value == 0)) +
