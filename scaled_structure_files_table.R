857eff3394c2ca6b331f91d422fc159a18b0e2ea
Alexander Davis <alexlovesmolecules@gmail.com>
2024-11-19 14:30:15 -0500

    Fix typos in last commit

--- scaled_structure_files_table.R
+++ scaled_structure_files_table.R
@@ -2,22 +2,22 @@ library(readr)
 library(dplyr)
 
 selected_structures_unscaled <- read_csv('selected_structure_files_unscaled.csv',
-        col_types = cols(.default = col_character())
+        col_types = cols(.default = col_character()))
 
 scaled_structure_data_unannotated <- read_csv('lattice_constant_structures.csv',
         col_types = cols(structure_id = col_character(),
                          unscaled_structure_id = col_character(),
                          scale_number = col_integer(),
                          scale = col_double(),
-                         structure_file_path = col_character())
+                         structure_file_path = col_character()))
 
 annotations <- selected_structures_unscaled |>
     select(-structure_file_path) |>
-    reneame(unscaled_structure_id = structure_id)
+    rename(unscaled_structure_id = structure_id)
 
 # Making the columns match before combining into a single table
 scaled_structure_data <- scaled_structure_data_unannotated |>
-    left_join(annotations, by = unscaled_structure_id)
+    left_join(annotations, by = 'unscaled_structure_id')
 equilibrium_structures <- selected_structures_unscaled |>
     mutate(scale_number = 0, scale = 1, unscaled_structure_id = structure_id)
 # Combining into a single table
