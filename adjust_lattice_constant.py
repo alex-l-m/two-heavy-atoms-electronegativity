1fde21fac3eb0bbbc006e7804b48bb10b8f7b36d
Alexander Davis <alexlovesmolecules@gmail.com>
2024-11-19 11:51:53 -0500

    Script to make scaled versions of structures

---
+++ adjust_lattice_constant.py
@@ -0,0 +1,41 @@
+'''Read input files for the structures I'm going to be simulating, and create
+versions with modified lattice constants'''
+
+import csv
+import os
+import shutil
+import pandas as pd
+from enutils import read_structure
+
+outdir = 'scaled_structures'
+# Create the output directory. If it already exists, erase it
+try:
+    os.mkdir(outdir)
+except FileExistsError:
+    shutil.rmtree(outdir)
+    os.mkdir(outdir)
+
+outtbl_path = 'lattice_constant_structures.csv'
+# Write header row
+with open(outtbl_path, 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['structure_id', 'unscaled_structure_id',
+        'scale_number', 'scale', 'structure_file_path'])
+
+intbl = pd.read_csv('selected_structure_files.csv')
+for row in intbl.itertuples():
+    unscaled_structure_id = row.structure_id
+    for scale_number, scale in zip(range(4), [.9, .95, 1.05, 1.1]):
+        # Create a new structure id by adding the scale number
+        structure_id = f'{unscaled_structure_id}_scale_{scale_number}'
+        # Load the original structure
+        crystal = read_structure(row.structure_file_path)
+        # Modify the lattice constant
+        crystal.set_cell(crystal.cell * scale, scale_atoms = True)
+        # Save the modified structure
+        outstructure_path = os.path.join(outdir, f'{structure_id}.xyz')
+        crystal.write(outstructure_path, format = 'extxyz')
+        with open(outtbl_path, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow([structure_id, unscaled_structure_id,
+                scale_number, scale, outstructure_path])
