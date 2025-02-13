31959a0061dec59c172491b2f08ebf8e1c4a0509
Alexander Davis <alexlovesmolecules@gmail.com>
2025-02-13 18:48:29 -0500

    Added niagara-specific module commands

--- analyze_cp2k_cube.sh
+++ analyze_cp2k_cube.sh
@@ -1,3 +1,8 @@
+module load NiaEnv/2022a
+module load gcc/11.3.0 openmpi/4.1.4+ucx-1.11.2
+module load cp2k/2024.2
+module load r
+
 # Derivative cube files
 python cube_diff.py density_derivatives cube_file_path
 python cube_diff.py vdiff pot_file_path
