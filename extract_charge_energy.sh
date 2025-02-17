9d5a87c3058ba9b1868558c4257f7bfef3fecde2
Alexander Davis <alexlovesmolecules@gmail.com>
2025-02-17 15:21:08 -0500

    Package extracted results into a tarball for convenience

--- extract_charge_energy.sh
+++ extract_charge_energy.sh
@@ -17,3 +17,8 @@ parallel -j $(nproc) < bader_commands.sh
 cd ..
 python parse_bader.py
 
+# Package into a tarball
+# This step isn't necessary for further analysis, I'm just including it because
+# I usually run the script on a remote server for the subsequent steps locally
+# and this gives me a single file to transfer
+tar -czf results.tar.gz *.csv *.csv.gz
