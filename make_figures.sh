c061b49d84114343814642cf491867c109e5b4fa
Alexander Davis <alexlovesmolecules@gmail.com>
2025-02-21 20:26:58 -0500

    Tikz diagram of a circuit for the intro figure

---
+++ make_figures.sh
@@ -0,0 +1,13 @@
+# Compile tikz image that is going to be used for a figure
+
+# File containing the tikz image
+IMGBASE=da_circuit
+TIKZFILE=$IMGBASE.tex
+
+# Compile with pdflatex
+pdflatex $TIKZFILE
+PDFFILE=$IMGBASE.pdf
+
+# Convert pdf to png with Poppler
+PNGFILE=$IMGBASE.png
+pdftoppm -png -r 300 $PDFFILE > $PNGFILE
