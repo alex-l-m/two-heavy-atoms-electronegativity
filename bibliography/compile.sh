28683e54cb6e09a042b640362bff4838fadf74dc
Alexander Davis <alexlovesmolecules@gmail.com>
2025-10-25 15:53:46 -0400

    Bibliography compilation script that works whether or not it has been run before

--- bibliography/compile.sh
+++ bibliography/compile.sh
@@ -1,3 +1,6 @@
-pdflatex display_annotated_bibliography.tex
+# Run only if "display_annotated_bibliography.aux" does not exist
+if [ ! -f display_annotated_bibliography.aux ]; then
+    pdflatex display_annotated_bibliography.tex
+fi
 bibtex display_annotated_bibliography.aux
 pdflatex display_annotated_bibliography.tex
